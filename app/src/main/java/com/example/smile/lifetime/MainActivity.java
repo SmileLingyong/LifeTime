74b4201d10a19042b8e97d4327bdd7f171356c00
smile <smilelingyong@163.com>
2017-03-28 11:14:54 +0800

    First commit

---
+++ app/src/main/java/com/example/smile/lifetime/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.smile.lifetime;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    //结束启动界面,转换到主页
+    public static void start(Activity activity){
+        activity.startActivity(new Intent(activity, MainActivity.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("生活点滴");
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        navView.setCheckedItem(R.id.nav_home);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_news:
+                        mDrawerLayout.closeDrawers();
+                        Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_theme:
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_aboutme:
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_setting:
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    default:
+                        mDrawerLayout.closeDrawers();
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_item, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.search:
+                Toast.makeText(this, "You want to search", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/example/smile/lifetime/NewsActivity.java
@@ -0,0 +1,104 @@
+package com.example.smile.lifetime;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+import util.HttpUtil;
+
+/**
+ * Created by lly54 on 2017/3/27.
+ */
+
+public class NewsActivity extends AppCompatActivity {
+
+    private static final int ANIMATION_DURATION = 4000;
+    private static final float SCALE_END = 1.13F;
+    private ImageView bingPicImg;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        //隐藏状态栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+        //看缓存中是否有图片
+        bingPicImg = (ImageView) findViewById(R.id.everyday_news);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String bingPic = prefs.getString("bing_pic", null);
+
+
+
+        if (bingPic != null) {
+            loadBingPic();
+            Glide.with(this).load(bingPic).into(bingPicImg);
+            animateImage();
+            Log.d("not null URL", bingPic);
+        } else {
+            loadBingPic();
+        }
+
+    }
+
+    private void loadBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(NewsActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                Log.d("URL", "bing_pic");
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(NewsActivity.this).load(bingPic).into(bingPicImg);
+                        animateImage();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    //设置每日一图动画效果
+    private void animateImage() {
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(bingPicImg, "scaleX", 1f, SCALE_END);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(bingPicImg, "scaleY", 1f, SCALE_END);
+
+        AnimatorSet set = new AnimatorSet();
+        //设置动画的长度：通过上面定义的animatorX，animatorY来渐变长度
+        set.setDuration(ANIMATION_DURATION).play(animatorX).with(animatorY);
+        set.start();
+
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                NewsActivity.this.finish();
+            }
+        });
+
+    }
+}
